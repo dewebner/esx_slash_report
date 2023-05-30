486e8b89b814469f01069b2ddae4fd821a70f39c
DeWebNer <72512544+dewebner@users.noreply.github.com>
2023-05-30 20:47:00 +0200

    Add files via upload

--- report.lua
+++ report.lua
@@ -11,13 +11,44 @@ RegisterCommand("report", function(source, args, rawCommand)
     local playerName = GetPlayerName(source)
 
     if reportMessage == "" then
-        TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Veuillez entrer un message de report." } })
+        --TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Veuillez entrer un message de report." } })
+        TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Utilisation: /report message" }, color = { 255, 0, 0 } })
         return
     end
 
     for _, playerId in ipairs(GetPlayers()) do
         if isAdminOrModerator(playerId) then
-            TriggerClientEvent("chat:addMessage", playerId, { args = { "[Report] " .. playerName, reportMessage } })
+            TriggerClientEvent("chat:addMessage", playerId, { args = { "[Report] " .. playerName, reportMessage }, color = { 255, 0, 0 } })
         end
+    end
+
+    for _, playerId in ipairs(GetPlayers()) do
+        if isAdminOrModerator(playerId) then
+            TriggerClientEvent("chat:addMessage", playerId, { args = { "[Report] " .. playerName .. " (" .. playerId .. ")", reportMessage }, color = { 255, 0, 0 } })
+        end
+    end
+end, false)
+
+-- Gère la commande de réponse /r id
+RegisterCommand("r", function(source, args, rawCommand)
+    local playerId = tonumber(args[1])
+    local responseMessage = table.concat(args, " ", 2)
+    local adminName = GetPlayerName(source)
+
+    if playerId == nil or responseMessage == "" then
+        TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Utilisation: /r id message" }, color = { 255, 0, 0 } })
+        return
+    end
+
+    if GetPlayerName(playerId) == nil then
+        TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Le joueur n'est pas connecté." }, color = { 255, 0, 0 } })
+        return
+    end
+
+    if isAdminOrModerator(source) then
+        TriggerClientEvent("chat:addMessage", playerId, { args = { "[Réponse Report] " .. adminName .. " (" .. source .. ")", responseMessage }, color = { 0, 0, 255 } })
+        TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Votre réponse a été envoyée." }, color = { 0, 255, 0 } })
+    else
+        TriggerClientEvent("chat:addMessage", source, { args = { "[Report]", "Vous n'êtes pas autorisé à utiliser cette commande." }, color = { 255, 0, 0 } })
     end
 end, false)
